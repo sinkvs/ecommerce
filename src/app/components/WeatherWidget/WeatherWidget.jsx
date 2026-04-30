f3d5d1c589195cb0b9d67ab5defb3fd42b207b93
Vladislav Sinkevich <sinkevichvs@vk.com>
2026-04-30 11:15:39 +0500

    (weather): базовая структура компонента с состоянием загрузки

--- src/app/components/WeatherWidget/WeatherWidget.jsx
+++ src/app/components/WeatherWidget/WeatherWidget.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+
 const WeatherWidget = () => {
-  return (
-    <div className="weather-widget">
-      {/*  */}
-    </div>
-  );
+    // Состояние isLoading. True - показываем лоадер, False - основной контент
+    const [isLoading, setIsLoading] = useState(true);
+
+    const renderContent = () => {
+        if (isLoading) {
+            return <p>Загрузка...</p>;
+        } else {
+            return <div>Будущий контент о погоде</div>;
+        }
+    };
+
+    return (
+        <div className="weather-widget">
+            {/*  */}
+            {renderContent()}
+        </div>
+    );
 };
 
 export default WeatherWidget;
